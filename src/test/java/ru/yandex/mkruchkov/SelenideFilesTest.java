38277dadecd21cf8c7925fbdbc36d8895930a389
Maxim Kruchkov <mkruchkov@ayist.games>
2022-01-19 18:53:55 +0300

    add 10-7 hw

---
+++ src/test/java/ru/yandex/mkruchkov/SelenideFilesTest.java
@@ -0,0 +1,41 @@
+package ru.yandex.mkruchkov;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class SelenideFilesTest {
+
+    private ClassLoader cl = SelenideFilesTest.class.getClassLoader();
+
+    @Test
+    void downloadTest() throws IOException {
+        open("https://github.com/assertj/assertj-core/blob/main/LICENSE.txt");
+        File downloadedFile = $("#raw-url").download();
+
+        try (InputStream is = new FileInputStream(downloadedFile)) {
+            assertThat(new String(is.readAllBytes(), StandardCharsets.UTF_8)).contains("\"Licensor\" shall mean the copyright owner or entity authorized");
+
+        }
+
+    }
+
+
+    @Test
+    void uploadFile() {
+        open("https://the-internet.herokuapp.com/upload");
+        $("input[type='file']").uploadFromClasspath("upload.txt");
+        $("#file-submit").click();
+        $("#uploaded-files").shouldHave(text("upload.txt"));
+    }
+}
